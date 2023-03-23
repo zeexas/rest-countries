bcb5238bce98ca9375cd779868579d4410710089
Valery Li <lee_hobb@rambler.ru>
2023-03-23 17:44:46 +0600

    create header and dark mode toggler

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,69 @@
-<script setup lang="ts">
-// import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-    This is the Header
-  </header>
+  <div :class="theme">
+    <the-header @toggle-theme="toggleTheme" :mode="theme"></the-header>
+  </div>
 </template>
 
-<style scoped>
+<script lang="ts">
+// setup
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  components: {
+    TheHeader
+  },
+  data() {
+    return {
+      reactive: {
+        isDark: false
+      }
+    }
+  },
+  provide() {
+    return {
+      theme: this.reactive
+    }
+  },
+  methods: {
+    toggleTheme() {
+      this.reactive.isDark = !this.reactive.isDark
+    }
+  },
+  computed: {
+    theme() {
+      return this.reactive.isDark ? 'Dark' : 'Light'
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap');
+body {
+  font-family: 'Nunito Sans', sans-serif;
+  color: hsl(200, 15%, 8%);
+  background-color: hsl(0, 0%, 95%);
+  /* font-size: 14px; */
+  /* font-weight: 600; */
+}
+.Dark {
+  color: white;
+  background-color: hsl(207, 26%, 17%);
+}
+.dark-bg {
+  background-color: hsl(207, 26%, 17%);
+}
+.dark-el {
+  background-color: hsl(209, 23%, 22%);
+}
+.light-input {
+  background-color: hsl(0, 0%, 52%);
+}
+.light-bg {
+  background-color: hsl(0, 0%, 98%);
+}
+.light-el {
+  background-color: hsl(0, 0%, 100%);
+}
 
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="flex justify-between items-center px-6 py-4 shadow-md"
+    :class="mode === 'Dark' ? 'dark-el' : 'light-el'"
+  >
+    <h2 class="text-2xl font-bold">Where in the world?</h2>
+    <button @click="toggleTheme" class="flex flex-row flex-nowrap items-center">
+      <MoonSVG v-if="!theme.isDark" />
+      <SunSVG v-if="theme.isDark" />
+      <p class="text-md font-bold ml-2">{{ mode === 'Dark' ? 'Light' : 'Dark' }} Mode</p>
+    </button>
+  </div>
+</template>
+
+<script>
+import MoonSVG from '../assets/moon-svg.vue'
+import SunSVG from '../assets/sun-svg.vue'
+
+export default {
+  inject: ['theme'],
+  components: {
+    MoonSVG,
+    SunSVG
+  },
+  props: ['mode'],
+  methods: {
+    toggleTheme() {
+      this.$emit('toggleTheme')
+    }
+  }
+}
+</script>
+
+<style scoped></style>
